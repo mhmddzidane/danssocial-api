e8f1288757e9a12babfd3ebaabff2729d8464c5b
mhmddzidane <muhammadzidane633@gmail.com>
2023-01-11 23:09:24 +0700

    first push

---
+++ controllers/post.js
@@ -0,0 +1,71 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+
+export const getPosts = (req, res) => {
+  const token = req.cookies.accessToken;
+  const userId = req.query.userId;
+  if (!token) return res.status(401).json("Not logged in");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token not valid");
+
+    console.log(userId);
+
+    const q =
+      userId !== "undefined"
+        ? `SELECT p.*, u.id AS userId, name,profilePic FROM posts AS p JOIN users AS u ON (u.id = p.user_id) WHERE p.user_id = ? ORDER BY p.created_at DESC`
+        : `SELECT p.*, u.id AS userId, name,profilePic FROM posts AS p JOIN users AS u ON (u.id = p.user_id)
+  LEFT JOIN relationship AS r ON (p.user_id = r.followed_user_id) WHERE r.follower_user_id = ? OR p.user_id = ?
+  ORDER BY p.created_at DESC`;
+
+    const values =
+      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+
+    db.query(q, values, (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO posts(`description`, `img`, `created_at`, `user_id`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created.");
+    });
+  });
+};
+
+export const deletePost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "DELETE FROM posts WHERE `id`=? AND `user_id` = ?";
+
+    db.query(q, [req.params.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0)
+        return res.status(200).json("Post has been deleted.");
+      return res.status(403).json("You can delete only your post");
+    });
+  });
+};
---
+++ controllers/relationship.js
@@ -0,0 +1,49 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getRelationships = (req, res) => {
+  const q =
+    "SELECT follower_user_id FROM relationship WHERE followed_user_id = ?";
+
+  db.query(q, [req.query.followedUserId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res
+      .status(200)
+      .json(data.map((relationship) => relationship.follower_user_id));
+  });
+};
+
+export const addRelationship = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO relationship (`follower_user_id`,`followed_user_id`) VALUES (?)";
+    const values = [userInfo.id, req.body.userId];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Following");
+    });
+  });
+};
+
+export const deleteRelationship = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "DELETE FROM relationship WHERE `follower_user_id` = ? AND `followed_user_id` = ?";
+
+    db.query(q, [userInfo.id, req.query.userId], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Unfollow");
+    });
+  });
+};
