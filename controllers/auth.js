c6271fbc2cdd78745c3a4e6514c40ff695bb3b24
mhmddzidane <muhammadzidane633@gmail.com>
2023-08-01 13:54:44 +0700

    adjust register

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,13 +16,17 @@ export const register = (req, res) => {
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
     const q =
-      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+      "INSERT INTO users (`username`,`email`,`password`,`name`,'city`,`website`,`profilePic`,`coverPic`) VALUE (?)";
 
     const values = [
       req.body.username,
       req.body.email,
       hashedPassword,
       req.body.name,
+      "",
+      "",
+      "",
+      "",
     ];
 
     db.query(q, [values], (err, data) => {
