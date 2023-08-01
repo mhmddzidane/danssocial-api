9706d3b4ec49f3dee33b9779abceb146f8bb82c0
mhmddzidane <muhammadzidane633@gmail.com>
2023-08-01 11:53:25 +0700

    adjustment

--- controllers/post.js
+++ controllers/post.js
@@ -5,12 +5,12 @@ import moment from "moment";
 export const getPosts = (req, res) => {
   const token = req.cookies.accessToken;
   const userId = req.query.userId;
-  if (!token) return res.status(401).json("Not logged in");
+  console.log("============");
+  console.log(req.cookies);
+  // if (!token) return res.status(401).json("Not logged in");
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token not valid");
-
-    console.log(userId);
 
     const q =
       userId !== "undefined"
